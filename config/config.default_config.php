2d6571b57b352d14407d1f237af8eae80913bea1
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 14:27:15 -0500

    change implementation start

--- config/config.default_config.php
+++ config/config.default_config.php
@@ -17,6 +17,10 @@ class default_config{
 		
 		//MTE
 		$this->semaforos = array('Reprobado','De panzazo','Bien','Excelente','No Enlace','Poco confiable');
+
+		//Change.org
+		$this->change_api_key = '944f03ed498ad797d7aa93901b48cceaef444f49c6fdcc72caaf6d1801772f09';
+		$this->change_secret_token = 'c2a7dd3596774463d3138cc3be72ffb2d2c4611973ae5ff5304b489b8ec9f61e';
 	}
 }
 ?>
